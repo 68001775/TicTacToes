2dee34669fb046f1b3972413c8f42fd6b1e7e4ca
CMP_LuYoungs <CMP_LuYoungs@RM37-24>
2022-10-26 10:05:27 -0400

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+    'Lukah Youngs
+    '10/26/22
+    'Using a class for Tic Tac Toes
+    Sub Main()
+        Console.WriteLine("Tic Tac Toes By Lukah Youngs!")
+        Dim myboard As New TTTboard
+        myboard.resetboard()
+        Dim gameover As Boolean = False
+
+        Do
+            Dim valid As Boolean = False
+            Dim usermove As Integer
+            Dim input As String
+            Do
+                printboard(myboard)
+                Console.Write("enter the space you want to try")
+                input = Console.ReadLine
+                If (Integer.TryParse(input, usermove)) Then
+                    valid = myboard.usespace(usermove, "X")
+                    If Not valid Then
+                        Console.WriteLine("Invalid move")
+                    End If
+                Else
+                    Console.WriteLine("Please enter an integer")
+                End If
+            Loop While Not valid
+        Loop While Not gameover
+    End Sub
+
+
+    Sub printboard(theboard As TTTboard)
+        Console.WriteLine(theboard.ToString())
+        'Console.WriteLine("{0}|{1}|{2}", theboard.board(0), theboard.board(1), theboard.board(2))
+        'Console.WriteLine(" -----")
+        'Console.WriteLine("{0}|{1}|{2}", theboard.board(3), theboard.board(4), theboard.board(5))
+        'Console.WriteLine(" -----")
+        'Console.WriteLine("{0}|{1}|{2}", theboard.board(0), theboard.board(1), theboard.board(2))
+        'Console.WriteLine(" -----")
+
+    End Sub
+
+End Module
---
+++ TTTboard.vb
@@ -0,0 +1,58 @@
+﻿Public Class TTTboard
+    'Public boardsize As Integer = 9
+    Private Const boardsize As Integer = 9
+    Private board(boardsize - 1) As String
+
+    Public Sub New()
+
+        resetboard()
+
+    End Sub
+
+    ''' <summary>
+    ''' resets the board so that all spaces hold their numeric place (no Xs or Os)
+    ''' </summary>
+    Public Sub resetboard()
+
+        For i As Integer = 0 To boardsize - 1
+
+            board(i) = i.ToString
+
+
+        Next
+
+    End Sub
+
+
+    Public Overrides Function ToString() As String
+        Return board(0) & "|" & board(1) & "|" & board(2) & "|" & vbNewLine &
+            "-----" & vbNewLine &
+        board(3) & "|" & board(4) & "|" & board(5) & "|" & vbNewLine &
+        "-----" & vbNewLine &
+        board(6) & "|" & board(7) & "|" & board(8) & "|" & vbNewLine
+    End Function
+    ''' <summary>
+    ''' accepts an index on the board, verifies it is open and the if "X" or "O" is passed in, makes the move
+    ''' </summary>
+    ''' <param name="spaceindex"></param>
+    ''' <param name="XorO"></param>
+    ''' <returns>true if successfully placed, false if otherwise</returns> 
+    Public Function usespace(spaceindex As Integer, XorO As String)
+        XorO = XorO.ToUpper.Trim
+        If XorO <> "X" AndAlso XorO <> "O" Then
+            Return False
+        End If
+
+        If spaceindex < 0 OrElse spaceindex >= board.Length Then
+            Return False
+        End If
+
+        If board(spaceindex) <> spaceindex.ToString Then
+            Return False
+        End If
+
+        board(spaceindex) = XorO
+        Return True
+
+    End Function
+End Class
